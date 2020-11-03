a681574f9a77f46c5639996b6b44c0d765fb2215
qiqimonkey4300 <yueqi04@gmail.com>
2020-11-03 15:25:32 -0500

    size, add, get, set, resize added, compiles sucessfully, tested

--- SuperArray.java
+++ SuperArray.java
@@ -4,5 +4,38 @@ public class SuperArray {
   private int size;
 
   public SuperArray() {
+    data = new String[10];
+    size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean add(String element) {
+    if (size == data.length) {
+      resize();
+    }
+    data[size] = element;
+    size++;
+    return true;
+  }
+
+  public String get(int index) {
+    return data[index];
+  }
+
+  public String set(int index, String element) {
+    String old = get(index);
+    data[index] = element;
+    return old;
+  }
+
+  private void resize() {
+    String[] replace = new String[size + 10];
+    for (int i = 0; i < size; i++) {
+      replace[i] = data[i];
+    }
+    data = replace;
   }
 }
